d5acaf6339658259e47708e1f5a0059b51ac9478
Damien Plower <a00279368@student.ait.ie>
2021-02-20 11:46:04 +0000

    Initial data.sql with sample insert to player

---
+++ src/main/resources/data.sql
@@ -0,0 +1,2 @@
+insert into player (memberbershipId,firstname,lastname,dateOfBirth,image,qrCode,clubId,activeMembership,trustScore)
+values (123,'Joe','Bloggs','01-Jan-1970','myimage.jpeg','testqrcode',555,true,100);
