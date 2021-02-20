7c31f8a6c9cdec7f110a210c203d080bf1de01eb
Damien Plower <a00279368@student.ait.ie>
2021-02-20 21:52:30 +0000

    Adding Create new Player functionality

--- src/main/java/com/verificationapplication/poc/controllers/PlayerController.java
+++ src/main/java/com/verificationapplication/poc/controllers/PlayerController.java
@@ -1,15 +1,18 @@
 package com.verificationapplication.poc.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.verificationapplication.poc.dataobjects.Player;
 import com.verificationapplication.poc.dataobjects.Team;
 import com.verificationapplication.poc.dataobjects.VerificationInput;
 import com.verificationapplication.poc.repositories.PlayerRepository;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class PlayerController {
@@ -38,6 +41,13 @@ public class PlayerController {
 		}else{
 			System.out.println("Player for membershipId [" + membershipId + "] Does not exist");
 		}
+	}
+
+	@PostMapping("players/")
+	public ResponseEntity createPlayer(@RequestBody Player newPlayer){
+		playerRepository.save(newPlayer);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/").query("membershipId={keyword}").buildAndExpand(newPlayer.getMembershipId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 	
