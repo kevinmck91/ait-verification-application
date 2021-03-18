6f52ae89d35b2784f6f39dabafe8f4f3b0c676c3
Damien Plower <a00279368@student.ait.ie>
2021-03-18 21:11:38 +0000

    FaceCompare VerificationCode

--- src/main/java/com/verificationapplication/poc/controllers/VerificationController.java
+++ src/main/java/com/verificationapplication/poc/controllers/VerificationController.java
@@ -1,5 +1,11 @@
 package com.verificationapplication.poc.controllers;
 
+import com.amazonaws.ResponseMetadata;
+import com.amazonaws.services.rekognition.model.CompareFacesResult;
+import com.amazonaws.services.rekognition.model.transform.CompareFacesResultJsonUnmarshaller;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.zxing.NotFoundException;
 import com.google.zxing.WriterException;
 import com.verificationapplication.poc.dataobjects.FailedVerificationReasons;
@@ -9,13 +15,25 @@ import com.verificationapplication.poc.dataobjects.VerificationOutput;
 import com.verificationapplication.poc.qr_utils.ReadQRCode;
 import com.verificationapplication.poc.repositories.PlayerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.util.UriBuilderFactory;
 
+
+import java.io.File;
 import java.io.IOException;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @RestController
 public class VerificationController {
@@ -113,5 +131,35 @@ public class VerificationController {
 		
 		return result;
 	}
-	
-}
+
+
+	@PostMapping("faceCompare")
+	public ResponseEntity<Object> compareFaces(@RequestParam("image1") MultipartFile multipartFile1, @RequestParam("image2") MultipartFile multipartFile2){
+		String url = "http://18.205.162.30:8082/faceCompare";
+		RestTemplate restTemplate = new RestTemplate();
+
+		Resource invoicesResource1 = multipartFile1.getResource();
+		Resource invoicesResource2 = multipartFile2.getResource();
+		LinkedMultiValueMap<String, Object> parts = new LinkedMultiValueMap<>();
+		parts.add("image1", invoicesResource1);
+		parts.add("image2", invoicesResource2);
+		HttpHeaders httpHeaders = new HttpHeaders();
+		httpHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+		HttpEntity<LinkedMultiValueMap<String, Object>> httpEntity = new HttpEntity<>(parts, httpHeaders);
+
+		//ResponseEntity<CompareFacesResult> result = restTemplate.postForEntity(url, httpEntity, CompareFacesResult.class);
+		ResponseEntity<Object> result = restTemplate.postForEntity(url, httpEntity, Object.class);
+
+	//	CompareFacesResult result1 = new CompareFacesResult();
+		System.err.println(result.getBody());
+		String body = result.getBody();
+		ResponseMetadata responseMetadata = new ResponseMetadata(result.getBody());
+
+
+		return result;
+	}
+
+
+
+
+	}
