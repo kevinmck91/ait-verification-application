e8f7df6a97c9737bd159de51ef124f284aaadb5c
Damien Plower <a00279368@student.ait.ie>
2021-03-21 20:00:27 +0000

    Commit changes for loading the Player Image and removing the QR Code

--- src/main/java/com/verificationapplication/poc/PocApplication.java
+++ src/main/java/com/verificationapplication/poc/PocApplication.java
@@ -1,35 +1,14 @@
 package com.verificationapplication.poc;
 
-import java.util.GregorianCalendar;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.verificationapplication.poc.dataobjects.Player;
-import com.verificationapplication.poc.repositories.PlayerRepository;
 
 @SpringBootApplication
-public class PocApplication implements CommandLineRunner {
-	
-	@Autowired
-    private PlayerRepository playerRepository;
+public class PocApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PocApplication.class, args);
 	}
-	
-	public void run(String... args) {
-		ObjectMapper mapper = new ObjectMapper();
-		mapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false);
-		// Add all players to the database manually 
-		// TODO convert this to a script
-		playerRepository.save(new Player(789, "firstname", "lastname", new GregorianCalendar(2013,10,28).getTime(), "image", "qrCode", 123, true, 21));
-		playerRepository.save(new Player(321, "firstname", "lastname", new GregorianCalendar(2013,10,27).getTime(), "image", "qrCode", 123, true, 21));
-		playerRepository.save(new Player(456, "firstname", "lastname", new GregorianCalendar(2013,10,26).getTime(), "image", "qrCode", 123, true, 21));
 
-	}
 }
--- src/main/java/com/verificationapplication/poc/dataobjects/Player.java
+++ src/main/java/com/verificationapplication/poc/dataobjects/Player.java
@@ -1,9 +1,7 @@
 package com.verificationapplication.poc.dataobjects;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.sql.Blob;
 import java.util.Date;
 
 @Entity
@@ -11,86 +9,95 @@ public class Player {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private 	int 	id;
-	private 	int 	membershipId;
-	private 	String 	firstname;
-	private 	String 	lastname;
-	private 	Date dateOfBirth;
-	private 	String 	image;
-	private 	String 	qrCode;
-	private 	int 	clubId;
-	private 	boolean activeMembership;
-	private 	int 	trustScore;
+	private int id;
+	private int membershipId;
+	private String firstname;
+	private String lastname;
+	private Date dateOfBirth;
+	@Lob
+	private Blob image;
+	private int clubId;
+	private boolean activeMembership;
+	private int trustScore;
 
 	
 	//TODO look up linter
 	public Player(){
 
 	}
-	
-	public Player(int membershipId, String firstname, String lastname, Date dateOfBirth, String image,
-			String qrCode, int clubId, boolean activeMembership, int trustScore) {
+
+	public Player(int id, int membershipId, String firstname, String lastname, Date dateOfBirth, Blob image, int clubId, boolean activeMembership, int trustScore) {
+		this.id = id;
 		this.membershipId = membershipId;
 		this.firstname = firstname;
 		this.lastname = lastname;
 		this.dateOfBirth = dateOfBirth;
 		this.image = image;
-		this.qrCode = qrCode;
 		this.clubId = clubId;
 		this.activeMembership = activeMembership;
 		this.trustScore = trustScore;
 	}
+
 	public int getMembershipId() {
 		return membershipId;
 	}
+
 	public void setMembershipId(int membershipId) {
 		this.membershipId = membershipId;
 	}
+
 	public String getFirstname() {
 		return firstname;
 	}
+
 	public void setFirstname(String firstname) {
 		this.firstname = firstname;
 	}
+
 	public String getLastname() {
 		return lastname;
 	}
+
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
+
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
+
 	public void setDateOfBirth(Date dateOfBirth) {
 		this.dateOfBirth = dateOfBirth;
 	}
-	public String getImage() {
+
+	public Blob getImage() {
 		return image;
 	}
-	public void setImage(String image) {
+
+	public void setImage(Blob image) {
 		this.image = image;
 	}
-	public String getQrCode() {
-		return qrCode;
-	}
-	public void setQrCode(String qrCode) {
-		this.qrCode = qrCode;
-	}
+
 	public int getClubId() {
 		return clubId;
 	}
+
 	public void setClubId(int clubId) {
 		this.clubId = clubId;
 	}
+
 	public boolean isActiveMembership() {
 		return activeMembership;
 	}
+
 	public void setActiveMembership(boolean activeMembership) {
 		this.activeMembership = activeMembership;
 	}
+
 	public int getTrustScore() {
 		return trustScore;
 	}
+
 	public void setTrustScore(int trustScore) {
 		this.trustScore = trustScore;
 	}
@@ -102,7 +109,7 @@ public class Player {
 	@Override
 	public String toString() {
 		return "Player [membershipId=" + membershipId + ", firstname=" + firstname + ", lastname=" + lastname
-				+ ", dateOfBirth=" + dateOfBirth + ", image=" + image + ", qrCode=" + qrCode + ", clubId=" + clubId
+				+ ", dateOfBirth=" + dateOfBirth + ", image=" + image + ", clubId=" + clubId
 				+ ", activeMembership=" + activeMembership + ", trustScore=" + trustScore + "]";
 	}
 
